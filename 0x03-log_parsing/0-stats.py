faa98ecb3b10acfdced66bbbd334c8e073be609f
vickouma77 <Vickprestone20@gmail.com>
2023-07-20 20:19:35 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,29 +11,3 @@ def print_stats(file_size, status_codes):
     for key, value in sorted(status_codes.items()):
         if value:
             print("{}: {}".format(key, value))
-
-
-if __name__ == "__main__":
-    file_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
-    counter = 0
-    try:
-        for line in sys.stdin:
-            counter += 1
-            data = line.split()
-            try:
-                file_size += int(data[-1])
-            except:
-                pass
-            try:
-                if data[-2] in status_codes:
-                    status_codes[data[-2]] += 1
-            except:
-                pass
-            if counter % 10 == 0:
-                print_stats(file_size, status_codes)
-        print_stats(file_size, status_codes)
-    except KeyboardInterrupt:
-        print_stats(file_size, status_codes)
-        raise
