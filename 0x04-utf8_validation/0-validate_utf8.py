4824d8d06dd665a1d170f2179655fbfe63dce409
vickouma77 <Vickprestone20@gmail.com>
2023-07-27 11:23:19 +0300

    UTF-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,18 +11,18 @@ def validUTF8(data):
     """
     byte_count = 0
 
-    for num in data:
+    for i in data:
         if byte_count == 0:
-            if (num >> 5) == 0b110:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
                 byte_count = 1
-            elif (num >> 4) == 0b1110:
+            elif i >> 4 == 0b1110:
                 byte_count = 2
-            elif (num >> 3) == 0b11110:
+            elif i >> 3 == 0b11110:
                 byte_count = 3
-            elif (num >> 7):
+            elif i >> 7 == 0b1:
                 return False
         else:
-            if (num >> 6) != 0b10:
+            if i >> 6 != 0b10:
                 return False
             byte_count -= 1
     return byte_count == 0
